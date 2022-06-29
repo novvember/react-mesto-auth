33c2b21cf952664634ac24ac1dc211794019079b
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-29 16:59:56 +0300

    feat: настроить управляемые компоненты в формах Register и Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
+import React from 'react';
 import {Link} from 'react-router-dom';
 
 import Header from './Header';
 
-function Login() {
+function Login({onSubmit}) {
+  const [inputs, setInputs] = React.useState({
+    email: '',
+    password: '',
+  });
+
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setInputs(state => ({...state, [name]: value}));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(inputs);
+  }
+
   return (
     <>
       <Header>
@@ -12,16 +29,24 @@ function Login() {
       <main>
         <div className="login content__element">
           <h2 className="login__title">Вход</h2>
-          <form className="login__form">
+          <form className="login__form" onSubmit={handleSubmit}>
             <input
               type="email"
               className="login__input"
               placeholder="Email"
+              name="email"
+              value={inputs.email}
+              onChange={handleChange}
+              required
             />
             <input
               type="password"
               className="login__input"
               placeholder="Пароль"
+              name="password"
+              value={inputs.password}
+              onChange={handleChange}
+              required
             />
             <button rype="submit" className="login__submit-button">Войти</button>
           </form>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Header from './Header';
 
-function Register() {
-  return (
+function Register({onSubmit}) {
+  const [inputs, setInputs] = React.useState({
+    email: '',
+    password: '',
+  });
 
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setInputs(state => ({...state, [name]: value}));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(inputs);
+  }
+
+  return (
     <>
       <Header>
         <Link to="/sign-in" className="header__menu-item">Войти</Link>
@@ -13,16 +29,24 @@ function Register() {
       <main>
         <div className="login content__element">
           <h2 className="login__title">Регистрация</h2>
-          <form className="login__form">
+          <form className="login__form" onSubmit={handleSubmit}>
             <input
               type="email"
               className="login__input"
               placeholder="Email"
+              name="email"
+              value={inputs.email}
+              onChange={handleChange}
+              required
             />
             <input
               type="password"
               className="login__input"
               placeholder="Пароль"
+              name="password"
+              value={inputs.password}
+              onChange={handleChange}
+              required
             />
             <button rype="submit" className="login__submit-button">Зарегистрироваться</button>
           </form>
